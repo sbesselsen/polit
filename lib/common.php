88d363335e070354c63fc9e43bd3e641bbbfcd75
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2011-08-31 15:25:16 +0200

    Basisfunctionaliteit ingebouwd. Werkt heel aardig

---
+++ lib/common.php
@@ -0,0 +1,128 @@
+<?php
+function p_common_run($remote_cmd) {
+    p_remote_run($remote_cmd);
+}
+
+function p_common_sync() {
+    $args = func_get_args();
+    $from = array_shift($args);
+    $to = array_shift($args);
+    $options = _p_read_options($args);
+    if (!empty ($options['exclude'])) {
+        $options['exclude'] = array_map('trim', array_filter(explode(',', $options['exclude'])));
+    }
+    p_sync($from, $to, $options);
+}
+
+function p_remote_run($remote_cmd, $server = null) {
+    if ($server === null) {
+        foreach (p_servers() as $server) {
+            if ($server !== null) {
+                p_remote_run($remote_cmd, $server);
+            }
+        }
+        return;
+    }
+    
+    $cmd = array ('ssh');
+    if (!empty ($server['port'])) {
+        $cmd[] = "-p{$server['port']}";
+    }
+    if (empty ($server['ssh_key'])) {
+        p_log("Skipping {$server['host']}: no SSH key");
+    }
+    $cmd[] = "-i " . p_local_path($server['ssh_key']);
+    $conn = "";
+    if (!empty ($server['user'])) {
+        $conn .= "{$server['user']}@";
+    }
+    $conn .= $server['host'];
+    $cmd[] = $conn;
+    
+    $cmd[] = escapeshellarg(p_apply_vars($remote_cmd, $server));
+    
+    $cmd = implode(' ', $cmd);
+    list ($pp, $in, $out, $err) = p_shell_cmd($cmd, true);
+    while ($line = fgets($out, 2048)) {
+        p_output(p_log_server_id($server) . '> ' . $line);
+    }
+    fclose($out);
+    proc_close($pp);
+}
+
+/**
+ * Sync files from the specified source directory to the specified target directory.
+ * 
+ * Options:
+ * - delete: delete files that are in the target directory but not in the source? Defaults to false.
+ * - exclude: path, or array of paths, to exclude
+ * - cvs_exclude: exclude .svn, .git etc.? Defaults to true
+ * 
+ * @param string $src
+ * @param string $dest
+ * @param array $options
+ */
+function p_sync($src, $dest, array $options = array ()) {
+    $options += array (
+        'delete' => false,
+        'exclude' => array (),
+        'cvs_exclude' => true,
+    );
+    if ($options['exclude'] && !is_array($options['exclude'])) {
+        $options['exclude'] = array ($options['exclude']);
+    }
+    
+    $src_abs = p_local_path($src);
+    
+    foreach (p_servers() as $server) {
+        p_log_indent("Syncing {$src} to {$server['host']}:{$dest}");
+        
+        $dest_abs = p_apply_vars($dest, $server);
+        if (isset ($server['root_dir'])) {
+            $dest_abs = _p_path_merge($server['root_dir'], $dest_abs);
+        }
+        $dest_abs = $server['host'] . ':' . $dest_abs;
+        if (!empty ($server['user'])) {
+            $dest_abs = "{$server['user']}@{$dest_abs}";
+        }
+        
+        $args = array ('-avz');
+        $ssh_cmd = 'ssh';
+        if (!empty ($server['port'])) {
+            $ssh_cmd .= " -p{$server['port']}";
+        }
+        if (!empty ($server['ssh_key'])) {
+            $ssh_cmd .= " -i " . p_local_path($server['ssh_key']);
+        }
+        $args[] = '-e ' . escapeshellarg($ssh_cmd);
+        if ($options['delete']) {
+            $args[] = '--delete';
+        }
+        if ($options['cvs_exclude']) {
+            $args[] = '--cvs-exclude';
+        }
+        if ($options['exclude']) {
+            foreach (array_filter($options['exclude']) as $exclude) {
+                $args[] = '--exclude=' . p_apply_vars($exclude, $server);
+            }
+        }
+        
+        $args[] = $src_abs . '/';
+        $args[] = $dest_abs;
+        
+        list ($pp, $stdin, $stdout, $stderr) = p_shell_cmd('rsync ' . implode(' ', $args), true);
+        while ($line = fgets($stdout, 2048)) {
+            if (!$line = trim($line)) {
+                continue;
+            }
+            if (preg_match('(^(sending incremental|sent [0-9]|total size is))', $line)) {
+                continue;
+            }
+            p_log($line);
+        }
+        fclose($stdout);
+        proc_close($pp);
+        
+        p_log_unindent();
+    }
+}
---
+++ lib/core.php
@@ -0,0 +1,352 @@
+<?php
+/**
+ * Run Polit.
+ */
+function polit() {
+    _p_extension_load('common');
+    
+    // read options
+    $args = $_SERVER['argv'];
+    array_shift($args);
+    $options = _p_read_options($args);
+    $options += array (
+        'base_dir' => '.',
+    );
+    
+    // store the base dir
+    _p_base_dir($options['base_dir']);
+    
+    // usage help
+    if (isset ($options['h']) || isset ($options['help'])) {
+        _p_usage();
+        return;
+    }
+    
+    // set log level
+    if (isset ($options['v'])) {
+        _p_log_level(-1);
+    } else if (isset ($options['q'])) {
+        _p_log_level(1);
+    } else if (isset ($options['qq'])) {
+        _p_log_level(2);
+    }
+    
+    if (isset ($options['group'])) {
+        p_select_groups(array_map('trim', array_filter(explode(',', $options['group']))));
+    }
+    if (isset ($options['host'])) {
+        p_select_hosts(array_map('trim', array_filter(explode(',', $options['host']))));
+    }
+    
+    if (empty ($options['cmd'])) {
+        p_error("No command specified");
+        return;
+    }
+    $f = "polit_{$options['cmd']}";
+    if (!is_callable($f)) {
+        $f = "p_common_{$options['cmd']}";
+        if (!is_callable($f)) {
+            p_error("Command {$options['cmd']} does not exist");
+            return;
+        }
+    }
+    call_user_func_array($f, $options['cmd_args']);
+}
+
+/**
+ * Log a message to STDOUT.
+ * @param string $msg
+ */
+function p_log($msg) {
+    if (_p_log_level() <= 0) {
+        fwrite(STDOUT, _p_log_format('INFO', $msg));
+    }
+}
+
+/**
+ * Output a raw message to STDOUT.
+ * @param string $msg
+ */
+function p_output($msg) {
+    if (_p_log_level() <= 0) {
+        fwrite(STDOUT, $msg);
+    }
+}
+
+/**
+ * Log an error to STDOUT.
+ * @param string $err
+ */
+function p_error($err) {
+    if (_p_log_level() <= 1) {
+        fwrite(STDERR, _p_log_format('ERROR', $err, 'red'));
+    }
+}
+
+/**
+ * Indent the log output.
+ */
+function p_log_indent($msg = null) {
+    if (_p_log_level() <= -1) {
+        if ($msg) {
+            fwrite(STDOUT, _p_log_format('', $msg, 'dark_gray'));
+        }
+        _p_log_prefix("> ");
+    }
+}
+
+/**
+ * Unindent the log output.
+ */
+function p_log_unindent() {
+    if (_p_log_level() <= -1) {
+        _p_log_prefix('', "> ");
+    }
+}
+
+/**
+ * Make an absolute path, based on the local base_dir.
+ * @param string $path
+ * @return string
+ */
+function p_local_path($path) {
+    return _p_path_merge(_p_base_dir(), $path);
+}
+
+/**
+ * Run a shell command.
+ * @param string $cmd
+ * @param bool $pointer Return a process pointer? Defaults to false. If you do this, be sure to close it using proc_close().
+ */
+function p_shell_cmd($cmd, $pointer = false) {
+    $descriptorspec = array (
+        0 => array ('pipe', 'r'),
+        1 => array ('pipe', 'w'),
+        2 => array ('pipe', 'w'),
+    );
+    $pp = proc_open($cmd, $descriptorspec, $pipes);
+    $err = '';
+    while ($err_line = fgets($pipes[2], 2048)) {
+        $err .= $err_line;
+    }
+    if ($err) {
+        p_error(rtrim($err, "\n"));
+    }
+    if ($pointer) {
+        return array ($pp, $pipes[0], $pipes[1], $pipes[2]);
+    }
+    proc_close($pp);
+}
+
+function p_log_server_id(array $server) {
+    $name = '';
+    if (!empty ($server['name'])) {
+        $name = $server['name'];
+    } else if (!empty ($server['host'])) {
+        $name = $server['host'];
+    }
+    $length = 30;
+    return substr(str_pad($name, $length, " "), 0, $length);
+}
+
+/**
+ * Read options from the args array.
+ * @param array &$args  argv array. Options will be stripped from this array.
+ * @return array $options
+ */
+function _p_read_options(array &$args) {
+    $options = array ();
+    foreach ($args as $k => $arg) {
+        if (substr($arg, 0, 2) == '--') {
+            $arg = substr($arg, 2);
+            if (strpos($arg, '=')) {
+                list ($param, $value) = explode('=', $arg);
+                $options[$param] = $value;
+            } else {
+                $options[$arg] = true;
+            }
+            unset ($args[$k]);
+        } else if (substr($arg, 0, 1) == '-') {
+            $chars = str_split(substr($arg, 1));
+            $options += array_combine($chars, array_fill(0, sizeof($chars), true));
+            unset ($args[$k]);
+        } else {
+            $a = array_values($args);
+            $options['cmd'] = array_shift($a);
+            $options['cmd_args'] = $a;
+            break;
+        }
+    }
+    return $options;
+}
+
+function _p_path_merge($base, $path) {
+    if (substr($path, 0, 1) == '/') {
+        return $path;
+    } else {
+        $path = rtrim($base, '/') . '/' . $path;
+        if ($realpath = realpath($path)) {
+            return $realpath;
+        }
+        return $path;
+    }
+}
+
+function _p_base_dir($dir = null) {
+    static $base_dir = null;
+    if ($dir) {
+        $base_dir = $dir;
+    }
+    return $base_dir;
+}
+
+function _p_extension_load($extension) {
+    $dir = dirname(__FILE__);
+    require_once ("{$dir}/{$extension}.php");    
+}
+
+function _p_log_color($str, $color) {
+    static $colors = array (
+        'red' => '0;31',
+    );
+    if (isset ($colors[$color])) {
+        $str = "\033[{$colors[$color]}m{$str}\033[0m";
+    }
+    return $str;
+}
+
+function _p_log_format($label, $msg, $color = null) {
+    $str = str_pad("[{$label}]", 8, " ", STR_PAD_RIGHT);
+    $str .= '[' . date('Y-m-d H:i:s') . '] ';
+    $str .= _p_log_prefix();
+    $str .= $msg;
+    if ($color) {
+        $str = _p_log_color($str, $color);
+    }
+    $str .= "\n";
+    return $str;
+}
+
+function _p_log_prefix($push = '', $pop = '') {
+    static $prefix = '';
+    if ($pop) {
+        if (substr($prefix, -1 * strlen($pop)) == $pop) {
+            $prefix = substr($prefix, 0, -1 * strlen($pop));
+        }
+    }
+    if ($push) {
+        $prefix .= $push;
+    }
+    return $prefix;
+}
+
+function _p_log_level($set = null) {
+    static $level = 0;
+    if ($set !== null) {
+        $level = (int)$set;
+    }
+    return $level;
+}
+
+function _p_usage() {
+    fputs(STDOUT, file_get_contents(dirname(__FILE__) . '/usage.txt'));
+}
+
+function p_select_groups($set = null, $reset = false) {
+    static $groups = null;
+    if ($set !== null || $reset) {
+        $groups = $set;
+    }
+    return $groups;
+}
+
+function p_subselect_groups($set = null, $reset = false) {
+    static $groups = null;
+    if ($set !== null || $reset) {
+        $groups = $set;
+    }
+    return $groups;
+}
+
+function p_subselect_groups_reset() {
+    p_subselect_groups(null, true);
+}
+
+function p_select_hosts($set = null, $reset = false) {
+    static $hosts = null;
+    if ($set !== null || $reset) {
+        $hosts = $set;
+    }
+    return $hosts;
+}
+
+function p_apply_vars($cmd, array $vars) {
+    $replace = array ();
+    foreach ($vars as $k => $v) {
+        if (!is_array($v) && !is_object($v)) {
+            $replace[$k] = $v;
+        }
+    }
+    foreach ($replace as $from => $to) {
+        $cmd = str_replace('#{' . $from . '}', $to, $cmd);
+    }
+    return $cmd;
+}
+
+function p_servers() {
+    $groups = p_select_groups();
+    $groups_sub = p_subselect_groups();
+    $hosts = p_select_hosts();
+    $output = array ();
+    foreach (p_all_servers() as $server) {
+        if ($groups !== null && !array_intersect($server['group'], $groups)) {
+            continue;
+        }
+        if ($groups_sub !== null && !array_intersect($server['group'], $groups_sub)) {
+            continue;
+        }
+        if ($hosts !== null && !in_array($server['host'], $hosts)) {
+            continue;
+        }
+        $output[] = $server;
+    }
+    return $output;
+}
+
+function p_all_servers() {
+    static $config = null;
+    if ($config === null) {
+        $config = array ();
+        $c = _p_config_raw();
+        $groups = isset ($c['groups']) ? $c['groups'] : array ();
+        $servers = isset ($c['servers']) ? $c['servers'] : array ();
+        foreach ($servers as $server) {
+            $s_groups = isset ($server['group']) ? (is_array($server['group']) ? $server['group'] : array ($server['group'])) : array ();
+            $server['group'] = $s_groups;
+            $group_data = array ();
+            foreach ($s_groups as $group) {
+                if (isset ($groups[$group])) {
+                    $group_data = array_merge($group_data, $groups[$group]);
+                }
+            }
+            $server += $group_data;
+            $config[] = $server;
+        }
+    }
+    return $config;
+}
+
+function _p_config_raw() {
+    static $config = null;
+    if ($config === null) {
+        $config = array ();
+        if (file_exists($path = p_local_path("config/servers.yml"))) {
+            $data = file_get_contents($path);
+            $yaml = yaml_parse($data);
+            if (is_array($yaml)) {
+                $config = $yaml;
+            }
+        }
+    }
+    return $config;
+}
